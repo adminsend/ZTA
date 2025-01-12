38d2f12177eeae3280da062cb2275a198dd41fff
Andreas Huemmer <andhue@ADMINSEND.INTRANET>
2025-01-12 11:30:01 +0100

    fist population :)

---
+++ Networkagent/NetworkAgentDB.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "ControlplaneID" (
+  "CPID" INTEGER NOT NULL ON CONFLICT ABORT,
+  "ÍsLocal" integer NOT NULL ON CONFLICT ABORT DEFAULT no COLLATE BINARY,
+  PRIMARY KEY ("CPID")
+);
+
+CREATE TABLE "Integrity" (
+  "OSType" TEXT NOT NULL,
+  "OSTypeTimeStamp" DATE NOT NULL,
+  "OSBuild" TEXT NOT NULL,
+  "OSBuildTimeStamp" DATE NOT NULL,
+  "AVType" TEXT NOT NULL,
+  "AVTypeTimeStamp" DATE NOT NULL,
+  "AVBuild" TEXT NOT NULL,
+  "AVBuildTimeStamp" DATE NOT NULL,
+  "AVIsActive" integer NOT NULL,
+  "AVIsActiveTimeStamp" DATE NOT NULL,
+  "OSUpdatesPending" integer NOT NULL,
+  "OSUpdatesPendingTimeStamp" DATE NOT NULL,
+  "SystemIsManaged" integer NOT NULL,
+  "SystemIsManagedTimeStamp" DATE NOT NULL,
+  CONSTRAINT "OID"  REFERENCES "ObjectID" ("OID") ON DELETE CASCADE
+);
+
+CREATE TABLE "ObjectID" (
+  "OID" INTEGER NOT NULL ON CONFLICT ABORT,
+  "UpdateStamp" integer NOT NULL ON CONFLICT FAIL,
+  PRIMARY KEY ("OID")
+);
+
